1232953ec3f0e949a1a69530d5a55a73011cc788
Predrag777 <predragbrajovicSS@gmail.com>
2025-11-10 22:42:56 +0100

    Introduce basic animation for dog

---
+++ Assets/Skripte/Aliies/Dog/Dog.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Dog : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private Animator animator;
+    private bool isMoving = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+        {
+            StartCoroutine(performMove());
+        }
+    }
+
+    IEnumerator performMove()
+    {
+        float moveDuration = 2.0f; 
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < moveDuration)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            animator.SetBool("move", true);
+            Debug.Log("Dog is moving");
+            yield return null;
+        }       
+
+        animator.SetBool("move", false);
+        transform.Rotate(0, 180, 0);
+        yield return new WaitForSeconds(3.0f);
+    }
+    
+
+}
